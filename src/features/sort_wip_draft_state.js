2e7ab18a4b45fb651892cf433b1171c34c4723d4
Romain Zanon <romain.zanon@canal-plus.com>
2024-08-14 22:39:00 +0200

    sort - handle other lowercase

--- src/features/sort_wip_draft_state.js
+++ src/features/sort_wip_draft_state.js
@@ -5,17 +5,22 @@ function sortWipDraftState() {
   const getOriginalRootNodeMR = select(".mr-list");
   const getAllMrEntry = select.all(".merge-request", getOriginalRootNodeMR);
   const sortedMR = getAllMrEntry
+    // sort wip/draft
     .map((mrEntry) => {
       const text = select(".merge-request-title-text a", mrEntry).text;
       return { text, mrEntry };
     })
     .sort((a, b) => {
       const isDraftA = a.text.indexOf("Draft");
+      const isDraftA2 = a.text.indexOf("draft");
       const isWipA = a.text.indexOf("WIP");
+      const isWipA2 = a.text.indexOf("Wip");
       const isDraftB = b.text.indexOf("Draft");
+      const isDraftB2 = b.text.indexOf("draft");
       const isWipB = b.text.indexOf("WIP");
+      const isWipB2 = b.text.indexOf("Wip");
 
-      return isDraftA + isWipA - (isDraftB + isWipB);
+      return (isDraftA + isDraftA2 + isWipA + isWipA2) - (isDraftB + isDraftB2 + isWipB + isWipB2);
     })
     .map((elem) => elem.mrEntry);
   for (const node in getAllMrEntry) {
