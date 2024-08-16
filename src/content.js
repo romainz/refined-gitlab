fda8a2dd24e55520cf9f4c705dda462f92348485
Romain Zanon <romain.zanon@canal-plus.com>
2024-08-16 17:06:48 +0200

    sort by reviewer

--- src/content.js
+++ src/content.js
@@ -1,5 +1,6 @@
 
-async function main() {
+
+function sortMR() {
   const getOriginalRootNodeMR = document.querySelector(".mr-list")
   const getAllMrEntry = document.querySelectorAll(".merge-request");
   const sortedMR = Array.from(getAllMrEntry)
@@ -8,25 +9,44 @@ async function main() {
       const text = titleNode.getElementsByTagName("a")[0].text
       return { text, mrEntry };
     })
-    .sort((a, b) => {
-      const isDraftA = a.text.indexOf("Draft");
-      const isDraftA2 = a.text.indexOf("draft");
-      const isWipA = a.text.indexOf("WIP");
-      const isWipA2 = a.text.indexOf("Wip");
-      const isDraftB = b.text.indexOf("Draft");
-      const isDraftB2 = b.text.indexOf("draft");
-      const isWipB = b.text.indexOf("WIP");
-      const isWipB2 = b.text.indexOf("Wip");
+    // sort by without/with reviewer
+    .sort((mrA, mrB) => {
+      var reviewerA = 1;
+      if (! mrA.mrEntry.getElementsByClassName("issuable-reviewers").length) {
+        reviewerA = 0;
+      }
+      var reviewerB = 1;
+      if (! mrB.mrEntry.getElementsByClassName("issuable-reviewers").length) {
+        reviewerB = 0;
+      }
+      return reviewerA - reviewerB;
+    })
+    // sort by WIP/Draft
+    .sort((mrA, mrB) => {
+      const isDraftA = mrA.text.indexOf("Draft");
+      const isDraftA2 = mrA.text.indexOf("draft");
+      const isWipA = mrA.text.indexOf("WIP");
+      const isWipA2 = mrA.text.indexOf("Wip");
+      const isDraftB = mrB.text.indexOf("Draft");
+      const isDraftB2 = mrB.text.indexOf("draft");
+      const isWipB = mrB.text.indexOf("WIP");
+      const isWipB2 = mrB.text.indexOf("Wip");
 
       return (isDraftA + isDraftA2 + isWipA + isWipA2) - (isDraftB + isDraftB2 + isWipB + isWipB2);
     })
     .map((elem) => elem.mrEntry);
+  // Remove all previous MR displayed
   for (let mrEntry of getAllMrEntry) {
     mrEntry.remove();
   }
+  // Display sorted MR
   for (const node in sortedMR) {
     getOriginalRootNodeMR.appendChild(sortedMR[node]);
   }
+}
+
+async function main() {
+  sortMR();
 }
 
 main();
