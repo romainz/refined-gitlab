ca35d3e0ba6edbaa2c0dcef7ee19e0443d200142
Romain Zanon <romain.zanon@canal-plus.com>
2024-09-04 17:45:36 +0200

    add filters

--- src/content.js
+++ src/content.js
@@ -78,16 +78,44 @@ function sortMR() {
   }
 }
 
+function isCurrentUserMr(mr) {
+  const currentUserName = getCurrentUser()
+  // check author
+  const author = mr.getElementsByClassName("author-link")[0].getAttribute("href").replace("/", "")
+  var isUserMr = false;
+  if (author == currentUserName) {
+    isUserMr = true
+  } else {
+    // check assigned
+    if (isReviewing(mr)) {
+      const assignedAndReviewers = mr.getElementsByClassName("issuable-reviewers")[0].getElementsByClassName("author-link")
+      for (let user of assignedAndReviewers) {
+        const userName = user.getAttribute("href").replace("/", "")
+        if (userName == currentUserName) {
+          isUserMr = true
+          break;
+        }
+      }
+    }
+  }
+  return isUserMr;
+}
+
 function getCurrentUser() {
-  return document.getElementsByClassName("js-current-user")[0].getElementsByClassName("dropdown-light-content")[0].text.replace(" ", "").replace("@", "")
+  return document.getElementsByClassName("js-current-user")[0].getElementsByClassName("dropdown-light-content")[0].textContent.trim().replace("@", "")
+}
+
+function getAllMr() {
+  return document.querySelectorAll(".merge-request");
 }
 
 
 function browseMR() {
-  const getAllMrEntry = document.querySelectorAll(".merge-request");
+  const getAllMrEntry = getAllMr();
   var approved = 0
   var inReview = 0
   var unassigned = 0
+  var currentUserMR = 0
   for (let mr of getAllMrEntry) {
     // color background MR
     if (isDraft(mr)) {
@@ -114,9 +142,13 @@ function browseMR() {
     if (isReviewing(mr)) {
       createReviewerLabel(mr);
     }
+
+    if (isCurrentUserMr(mr)) {
+      currentUserMR = currentUserMR + 1
+    }
   }
 
-  addFilters(getAllMrEntry.length, unassigned, inReview, approved);
+  addFilters(getAllMrEntry.length, unassigned, inReview, approved, currentUserMR);
 }
 
 // Handle labels
@@ -177,15 +209,37 @@ function addP0Label(mr) {
   }
 }
 
-function addFilters(all, unassigned, inReview, approved) {
+function addFilters(all, unassigned, inReview, approved, currentUserMR) {
   const filtersGroup = document.getElementsByClassName("issues-filters")[0]
   const margin = "5"
 
-  // All
   createClickableLabel(filtersGroup, "All (" + all + ")", "#A9A9A9", "", margin);
   createClickableLabel(filtersGroup, "Unassigned (" + unassigned + ")", "rgba(208, 208, 208, 1)", "reviewer_id=None&draft=no", margin);
   createClickableLabel(filtersGroup, "In review (" + inReview + ")", "rgba(253, 172, 83, 1)", "reviewer_id=Any&draft=no&approved_by_usernames[]=None", margin);
   createClickableLabel(filtersGroup, "Approved (" + approved + ")", "#A0DAA9", "approved_by_usernames[]=Any", margin);
+
+  // add filter of the user
+  const currentUserName = getCurrentUser();
+  const labelHtml = "<span class=\"gl-label gl-label-sm\" style=\"margin:" + margin + "px\"><a class=\"gl-link gl-label-link\"><span class=\"gl-label-text gl-label-text-light\" data-container=\"body\" data-html=\"true\" style=\"background-color:#A9A9A9\">" + currentUserName + " (" + currentUserMR + ")</span></a></span>"
+  filtersGroup.insertAdjacentHTML("beforeend", labelHtml)
+  filtersGroup.children[filtersGroup.childElementCount - 1].addEventListener("click", function() {
+    displayCurrentUserMr();
+  });
+}
+
+function displayCurrentUserMr() {
+  const allMr = getAllMr();
+  const currentUserMRs = Array.from(allMr)
+    .filter(isCurrentUserMr);
+  // Remove all previous MR displayed
+  for (let mr of allMr) {
+    mr.remove();
+  }
+  const originalRootNodeMR = document.querySelector(".mr-list")
+  // Display current user mrs
+  for (let mr of currentUserMRs) {
+    originalRootNodeMR.appendChild(mr);
+  }
 }
 
 async function main() {
